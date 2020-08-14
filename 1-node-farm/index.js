38c33292a63b0e18f3fea4c597844f666b37fd06
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-14 11:32:11 -0700

    Basic file read and write

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
-const hello = 'Hello, world!';
-console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what what we know abou the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('fire written!');
