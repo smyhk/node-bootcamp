21b27bf1a7ad11bc9b97a3c5ca9453c6b90cd955
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-15 15:20:12 -0700

    Demo basic http server with rudimentary routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+/////////////////////////// FILES /////////////////////////////////
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -9,18 +12,41 @@ const fs = require('fs');
 // console.log('File written!');
 
 // Non-blocking, asynchronous way (in callback hell format)
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('Error! 🤬');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error! 🤬');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}}`, 'utf-8', (err) => {
-        console.log('Your file has been written 🤪');
-      });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}}`, 'utf-8', (err) => {
+//         console.log('Your file has been written 🤪');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!'); // Displays before file contents
+
+/////////////////////////// SERVER /////////////////////////////////
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    // Status and headers must be defined before response is sent
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'You-Fucked_Up': 'haha you fucked it away!',
     });
-  });
+    res.end('<h1>Page not found!</h1>');
+  }
+
+  res.end('Hello from the server!');
 });
-console.log('Will read file!'); // Displays before file contents
+
+server.listen(8000, '127.0.0.1', () => {
+  console.info('server is listening on port 8000');
+});
