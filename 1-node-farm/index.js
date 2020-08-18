041add278a98832b29ee4c1458830a0d5045e0ea
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-18 12:31:43 -0700

    Demonstrate the event loop and thread pool

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const url = require("url");
-const http = require("http");
-const slugify = require("slugify");
-const replaceTemplate = require("./modules/replaceTemplate");
+const fs = require('fs');
+const url = require('url');
+const http = require('http');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////// FILES /////////////////////////////////
 // Blocking, synchronous way
@@ -34,18 +34,18 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // Top-level code - executes only once; ok to use blocking
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
 
@@ -54,41 +54,41 @@ const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     const cardsHtml = dataObject
       .map((el) => replaceTemplate(tempCard, el))
-      .join(""); // Make into a string
+      .join(''); // Make into a string
 
     const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
 
     res.end(output);
 
     // Product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     const product = dataObject[query.id];
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
     // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(data);
 
     // Not found
   } else {
     // Status and headers must be defined before response is sent
     res.writeHead(404, {
-      "Content-Type": "text/html",
-      "You-Fucked-Up": "haha you fucked it away!",
+      'Content-Type': 'text/html',
+      'You-Fucked-Up': 'haha you fucked it away!',
     });
-    res.end("<h1>Page not found!</h1>");
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.info("server is listening on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.info('server is listening on port 8000');
 });
