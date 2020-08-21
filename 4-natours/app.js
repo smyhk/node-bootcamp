17c3453f780754517cd0bee7660b981d2a655727
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-21 11:27:37 -0700

    Finish basic CRUD operations

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -30,6 +30,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  // const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  //if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -47,6 +68,52 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  const index = tours.findIndex((el) => el.id === id);
+  const updatedTour = {
+    ...tour,
+    ...req.body,
+  };
+  tours[index] = updatedTour;
+
+  await fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).json({
+        message: 'Tour Updated',
+        data: {
+          updatedTour,
+        },
+      });
+    }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 const PORT = 3000;
