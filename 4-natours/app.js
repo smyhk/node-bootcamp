dcb7a280f511f94641582a811ba92ce5a6eab8db
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-21 00:08:32 -0700

    Send data to dummy database

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -25,6 +28,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
