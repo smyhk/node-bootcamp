d00766f073e68791210971f83744ada911475946
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-08-28 00:28:14 -0700

    Connect express to mongodb

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,11 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+//.then(() => console.log('DB connnection successful'));
+
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.info(`App running on port ${PORT}.`);
+mongoose.connection.once('open', () => {
+  console.log('DB connnection successful');
+  app.listen(PORT, () => {
+    console.info(`App running on port ${PORT}.`);
+  });
 });
