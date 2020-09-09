25fe6ce1bda76ecb9b6ac1405c5bc450c2c910a4
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-09-09 11:33:24 -0700

    Implement pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -38,7 +38,7 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
-      // sort('price', 'ratingsAverge')
+      // ex. sort('price', 'ratingsAverge')
     } else {
       query = query.sort('-createdAt');
     }
@@ -49,6 +49,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v -createdAt -updatedAt');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // Execute query
