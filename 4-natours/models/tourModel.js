ae56b37e91e023126c1916e63908f5bb0e15c1e6
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-09-13 12:28:38 -0700

    Use basic aggregation middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -112,6 +112,13 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// AGGREGATION middlware, this -> aggregation object
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
