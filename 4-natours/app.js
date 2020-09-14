94bc6456da31c3c2cd571fb417b1d7c725bc8da1
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-09-13 21:41:26 -0700

    Start implementing custom Error handler

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
@@ -24,10 +26,9 @@ app.use('/api/v1/users', userRouter);
 
 // Handle non-existant routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
