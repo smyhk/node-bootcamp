da07b7320f8cee08b017aa58571d35b6c8f2ff8b
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-09-22 12:17:44 -0700

    Create basic user

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,50 +1,43 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'A user must have name'],
-      trim: true,
-      maxlength: [40, 'A user name must have less than 41 characters'],
-      minlength: [10, 'A user name must have at least 10 characters'],
-      // validate: [validator.isAlpha, 'Tour name must only contain letters'],
-    },
-    email: {
-      type: String,
-      required: [true, 'A user must have name'],
-      unique: true,
-      lowercase: true,
-      trim: true,
-      maxlength: [40, 'An email must have less than 41 characters'],
-      minlength: [10, 'An email must have at least 10 characters'],
-      validate: [validator.isEmail, 'Please use a valid email address'],
-    },
-    password: {
-      type: String,
-      required: [true, 'A user must have a password'],
-      trim: true,
-      maxlength: [40, 'A password must have less than 41 characters'],
-      minlength: [8, 'A password must have at least 8 characters'],
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, 'Passwords must mtch'],
-      trim: true,
-      maxlength: [40, 'A password must have less than 41 characters'],
-      minlength: [8, 'A password must have at least 8 characters'],
-    },
-    photo: {
-      type: String,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have name'],
+    trim: true,
+    maxlength: [40, 'A user name must have less than 41 characters'],
+    minlength: [3, 'A user name must have at least 10 characters'],
+    // validate: [validator.isAlpha, 'Tour name must only contain letters'],
   },
-  {
-    timestamps: true,
-    // toJSON: { virtuals: true },
-    // toObject: { virtuals: true },
-  }
-);
+  email: {
+    type: String,
+    required: [true, 'A user must have name'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    maxlength: [40, 'An email must have less than 41 characters'],
+    // minlength: [10, 'An email must have at least 10 characters'],
+    validate: [validator.isEmail, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    //trim: true,
+    //maxlength: [40, 'A password must have less than 41 characters'],
+    //minlength: [8, 'A password must have at least 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    //trim: true,
+    //maxlength: [40, 'A password must have less than 41 characters'],
+    //minlength: [8, 'A password must have at least 8 characters'],
+  },
+  photo: {
+    type: String,
+  },
+});
 
 const User = mongoose.model('User', userSchema);
 
