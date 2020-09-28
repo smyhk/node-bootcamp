ec79ef0672e55be48deb94275e325dd2f5e5f0a5
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-09-28 13:23:48 -0700

    Implement rate-limiting on api

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,19 @@ const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
-// Middleware stack
+// GLOBAL middleware stack
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// TODO: adapt limiter to application
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
