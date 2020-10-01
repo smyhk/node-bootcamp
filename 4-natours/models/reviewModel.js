e2f2faa224d6494ba5656e6fff2bf1a446a429ee
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-10-01 12:00:29 -0700

    Define Review model

---
+++ 4-natours/models/reviewModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+// Reviews are modeled with parent referencing
+const reviewSchema = mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty!'],
+      maxlength: [250, 'A tour name must have less than 250 characters'],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1.0, 'Rating must be at least 1'],
+      max: [5.0, 'Rating cannot be more than 5'],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must have a user.'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now().toLocaleString(),
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
