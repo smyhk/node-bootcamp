e78e583ac2bd8b34e4f69ed8a326a25469ab54ad
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-10-08 14:02:35 -0700

    Finish geoLocation features

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -161,11 +161,11 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // AGGREGATION middlware, this -> aggregation object
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -22,6 +22,8 @@ router
     tourController.getMonthlyPlan
   );
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
