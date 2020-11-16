214ba1db6223d918ed646a1c721585f3f12773cf
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-11-16 12:22:29 -0800

    Process multiple images on tours

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -24,24 +24,28 @@ const multerFilter = (req, file, callback) => {
   } else {
     callback(new AppError('Filetypes must be images only', 400), false);
   }
-}
+};
 
 const uploader = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
 });
 
-exports.uploadUserPhoto = uploader.single('photo')
+exports.uploadUserPhoto = uploader.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.fileName = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({ quality: 90 }).toFormat('jpeg').toFile(`public/img/users/${req.file.fileName}`);
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.fileName}`);
 
   next();
-}
+});
 
 // Filters out unwanted fields
 const filterObj = (obj, ...allowedFields) => {
